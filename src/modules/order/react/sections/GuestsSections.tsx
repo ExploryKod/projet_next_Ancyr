1248f7920a4056a24637320b1c13a0424dcb5641
Amaury Franssen <amauryfranssen@gmail.com>
2023-08-05 15:39:29 +0200

    feat(order-sections): style - add form and buttons to order sections

--- src/modules/order/react/sections/GuestsSections.tsx
+++ src/modules/order/react/sections/GuestsSections.tsx
@@ -1,13 +1,49 @@
 "use client";
+import { Box, Grid, Typography, Button, FormControl, TextField, FormLabel } from "@mui/material";
 
 export const GuestsSection: React.FC<{}> = () => {
-  return <main>
- 
-    <h1>Guests</h1>
-  </main>;
+  return <Box sx={{ marginTop: 2 }}>
+    <Typography variant="h4">Guests</Typography>
+    <Grid rowSpacing={4}>
+        <Box sx={{ paddingTop: 2 }}>
+            <GuestRow />
+        </Box>
+    </Grid>
+    <Grid container rowSpacing={4} direction={"row"} alignItems={"center"} spacing={1} marginTop={2}>
+        <Grid item>
+            <Button variant="contained">Ajouter</Button>
+        </Grid>
+        <Grid item>
+            <Button variant="contained">Suivant</Button>
+        </Grid>
+    </Grid>
+  </Box>;
 };
 
 const GuestRow: React.FC<{}> = () => { 
-    return 
+    return (
+        <Box>
+            <Grid container direction={"row"} alignItems={"center"} spacing={1} >
+                <Grid item >
+                    <FormControl>
+                        <FormLabel>Prénom</FormLabel>
+                        <TextField/>
+                    </FormControl>
+                </Grid>
+                <Grid item >
+                    <FormControl>
+                        <FormLabel>Nom</FormLabel>
+                        <TextField/>
+                    </FormControl>
+                </Grid>
+                <Grid item >
+                    <FormControl>
+                        <FormLabel>Âge</FormLabel>
+                        <TextField/>
+                    </FormControl>
+                </Grid>
+            </Grid>
+        </Box>
+    )
 
 };
